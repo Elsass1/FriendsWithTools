2d069081b601d2ce17a0991cd53cf44dd0147f18
Brice <fromm.brice@gmail.com>
2024-06-27 07:25:56 +0200

    added heart icon

--- app/components/Liked.tsx
+++ app/components/Liked.tsx
@@ -1,13 +1,13 @@
-
 'use client';
 import React, { useState } from 'react';
 import { ToolCard as ToolType } from '../lib/types';
+import { HeartIcon } from '@heroicons/react/24/outline';
 
 interface LikedProps {
   tool: ToolType; // Define the type of the 'tool' prop
 }
 
-const Liked = ()=> {
+const Liked = () => {
   const [isLiked, setIsLiked] = useState<boolean>(false);
 
   const handleLike = () => {
@@ -22,11 +22,17 @@ const Liked = ()=> {
     // TODO wishlist.push(tool);
   };
 
-
   return (
     <div>
-      <div className="absolute top-2 right-2 cursor-pointer text-2xl" onClick={handleLike}>
-        {isLiked ? '❤️' : '🤍'}
+      <div
+        className='absolute top-2 right-2 cursor-pointer text-2xl'
+        onClick={handleLike}
+      >
+        {isLiked ? (
+          <HeartIcon className='fill-current h-8 w-8 text-red-400' />
+        ) : (
+          <HeartIcon className='h-8 w-8 text-red-400' />
+        )}
       </div>
     </div>
   );

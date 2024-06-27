270e4388629ada41ab69cdc14d5e29895ad8f22c
Brice <fromm.brice@gmail.com>
2024-06-27 17:13:04 +0200

    search bar

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -11,25 +11,33 @@ const SearchBar = () => {
   const searchParams = useSearchParams();
   const pathName = usePathname();
   const router = useRouter();
-  const handleChange = useDebouncedCallback((query: string) => {
-    const params = new URLSearchParams(searchParams);
-    if (query) params.set('query', query);
-    else params.delete('query');
+  // const handleChange = useDebouncedCallback((query: string) => {
+  //   const params = new URLSearchParams(searchParams);
+  //   if (query) params.set('query', query);
+  //   else params.delete('query');
 
-    router.replace(`${pathName}?${params.toString()}`);
-  }, 500);
+  //   router.replace(`${pathName}?${params.toString()}`);
+  // }, 500);
+  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    router.push();
+  };
 
   return (
     <div className='flex justify-center pt-6 pb-6 inset-x-0 top-0 fixed bg-white z-40 mb-5'>
       <div className='relative flex items-center justify-center w-11/12  '>
         <MagnifyingGlassIcon className='absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 transform stroke-2 cursor-pointer' />
-        <Input
-          onChange={(event) => handleChange(event.target.value)}
-          defaultValue={searchParams.get('query') ?? ''}
-          placeholder='What do you wish to rent?'
-          className='shadow-md pl-10 focus-visible:ring-offset-0 focus-visible:ring-0 rounded-3xl h-12 cursor-pointer'
-        />
+        <form onSubmit={handleSearch}>
+          <Input
+            onChange={(event) => handleChange(event.target.value)}
+            defaultValue={searchParams.get('query') ?? ''}
+            placeholder='What do you wish to rent?'
+            className='shadow-md pl-10 focus-visible:ring-offset-0 focus-visible:ring-0 rounded-3xl h-12 cursor-pointer'
+          />
+          <button className='bg-blue-50'>Search</button>
+        </form>
       </div>
+
     </div>
   );
 };
